8a9410dae9ce47cefeeea96d380e1166d3f9222c
Nick Moss <nickm@neu.local>
2014-08-18 09:21:43 -0600

    Fixes.

--- include/neu/NQueue.h
+++ include/neu/NQueue.h
@@ -67,15 +67,15 @@ namespace neu{
     typedef T value_type;
     typedef Allocator allocator_type;
     
-    typedef typename allocator_type::reference reference;
-    typedef typename allocator_type::const_reference const_reference;
+    typedef typename Queue::allocator_type::reference reference;
+    typedef typename Queue::allocator_type::const_reference const_reference;
     typedef typename Queue::iterator  iterator;
     typedef typename Queue::const_iterator const_iterator;
-    typedef typename allocator_type::size_type size_type;
-    typedef typename allocator_type::difference_type difference_type;
+    typedef typename Queue::allocator_type::size_type size_type;
+    typedef typename Queue::allocator_type::difference_type difference_type;
     
-    typedef typename allocator_type::pointer pointer;
-    typedef typename allocator_type::const_pointer const_pointer;
+    typedef typename Queue::allocator_type::pointer pointer;
+    typedef typename Queue::allocator_type::const_pointer const_pointer;
     typedef std::reverse_iterator<iterator> reverse_iterator;
     typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
     
