fa3b3f98d1fcf736f2d78faa9499d83d5e1430bc
Nick Moss <nickm@neu.local>
2014-08-19 19:47:59 -0600

    Add to nstr.

--- include/neu/nstr.h
+++ include/neu/nstr.h
@@ -347,7 +347,7 @@ namespace neu{
       return str_.substr(pos, n);
     }
     
-    nstr endStr(size_t pos){
+    nstr endStr(size_t pos) const{
       return str_.substr(pos, str_.length() - pos);
     }
     
