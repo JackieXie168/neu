48e813c442020700e0068721daaf5f511e9c0736
Nick Moss <neu@andrometa.net>
2014-09-07 14:16:55 -0600

    Fixes to nvar.

--- include/neu/nvar.h
+++ include/neu/nvar.h
@@ -2050,6 +2050,30 @@ namespace neu{
         case HeadSequence:
           h_.hs->s->pushBack(std::move(x));
           break;
+        case Set:{
+          Head hv;
+          hv.v = new nvec;
+          hv.v->emplace_back(std::move(x));
+          
+          Head hm;
+          hm.set = h_.set;
+          
+          h_.sm = new CSequenceMap(new nvar(Vector, hv), new nvar(Set, hm));
+          t_ = SequenceMap;
+          break;
+        }
+        case HashSet:{
+          Head hv;
+          hv.v = new nvec;
+          hv.v->emplace_back(std::move(x));
+          
+          Head hm;
+          hm.hset = h_.hset;
+          
+          h_.sm = new CSequenceMap(new nvar(Vector, hv), new nvar(HashSet, hm));
+          t_ = SequenceMap;
+          break;
+        }
         case Map:{
           Head hv;
           hv.v = new nvec;
